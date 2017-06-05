3ff68065476fcaa7b7e06c8c28ee4cffac087567
Fin Kingma <Kingma@zorgdomein.nl>
2017-06-05 10:40:57 +0200

    .

--- karma.conf.js
+++ karma.conf.js
@@ -13,7 +13,7 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine', 'pact'],
+    frameworks: ['jasmine', 'pact', 'sinon'],
     pact: {},
 
 
