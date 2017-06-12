251b5c5e29d39ecc2d7a7383322fe89fe7665a65
Fin Kingma <Kingma@zorgdomein.nl>
2017-06-12 15:35:52 +0200

    .

--- publish-pacts.js
+++ publish-pacts.js
@@ -1,13 +1,14 @@
 const pact = require('@pact-foundation/pact-node')
 const path = require('path')
 if (!process.env.PACTBROKERURL) throw new Error('Please specify where the pactbroker can be found with PACTBROKERURL');
+
 const opts = {
   pactUrls: [path.resolve(__dirname, './pacts/exploratorytestinggame-mapmakerapi.json')],
   pactBroker: process.env.PACTBROKERURL,
   //pactBrokerUsername: 'dXfltyFMgNOFZAxr8io9wJ37iUpY42M',
   //pactBrokerPassword: 'O5AIZWxelWbLvqMd8PkAVycBJh2Psyg1',
   //tags: ['prod', 'test'],
-  consumerVersion: '8.0.0'
+  consumerVersion: getDateTime()
 }
 
 pact.publishPacts(opts)
@@ -20,3 +21,30 @@ pact.publishPacts(opts)
   .catch(e => {
     console.log('Pact contract publishing failed: ', e)
   })
+
+  function getDateTime() {
+    var now     = new Date(); 
+    var year    = now.getFullYear();
+    var month   = now.getMonth()+1; 
+    var day     = now.getDate();
+    var hour    = now.getHours();
+    var minute  = now.getMinutes();
+    var second  = now.getSeconds(); 
+    if(month.toString().length == 1) {
+        var month = '0'+month;
+    }
+    if(day.toString().length == 1) {
+        var day = '0'+day;
+    }   
+    if(hour.toString().length == 1) {
+        var hour = '0'+hour;
+    }
+    if(minute.toString().length == 1) {
+        var minute = '0'+minute;
+    }
+    if(second.toString().length == 1) {
+        var second = '0'+second;
+    }   
+    var dateTime = year+month+day+hour+minute+second;   
+     return dateTime;
+}
