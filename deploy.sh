b5f264f65931b49911d2bd63e2c6074b1fdba5b4
Fin Kingma <Kingma@zorgdomein.nl>
2017-06-21 12:36:45 +0200

    now front also set to frankfurt

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ JQ="jq --raw-output --exit-status"
 
 configure_aws_cli(){
 	aws --version
-	aws configure set default.region us-east-1
+	aws configure set default.region eu-central-1
 	aws configure set default.output json
 }
 
@@ -20,40 +20,25 @@ deploy_cluster() {
         echo "Error updating service."
         return 1
     fi
-
-    # wait for older revisions to disappear
-    # not really necessary, but nice for demos
-    for attempt in {1..30}; do
-        if stale=$(aws ecs describe-services --cluster etg --services etg-front | \
-                       $JQ ".services[0].deployments | .[] | select(.taskDefinition != \"$revision\") | .taskDefinition"); then
-            echo "Waiting for stale deployments:"
-            echo "$stale"
-            sleep 5
-        else
-            echo "Deployed!"
-            return 0
-        fi
-    done
-    echo "Service update took too long."
-    return 1
+    return 0
 }
 
 make_task_def(){
 	task_template='[
 		{
 			"name": "frontend",
-			"image": "%s.dkr.ecr.us-east-1.amazonaws.com/front:%s",
+			"image": "%s.dkr.ecr.eu-central-1.amazonaws.com/front:%s",
 			"essential": true,
 			"memory": 200,
 			"cpu": 10,
 			"portMappings": [
 				{
 					"containerPort": 2000,
-					"hostPort": 2000
+					"hostPort": 0
 				}
 			],
             "environment" : [
-                { "name" : "MAPMAKERURL", "value" : "http://54.197.31.162:3000" }
+                { "name" : "MAPMAKERURL", "value" : "http://etg-balancer-862594806.eu-central-1.elb.amazonaws.com:3000" }
             ]
 		}
 	]'
@@ -62,8 +47,8 @@ make_task_def(){
 }
 
 push_ecr_image(){
-	eval $(aws ecr get-login --region us-east-1)
-	docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/front:$CIRCLE_SHA1
+	eval $(aws ecr get-login --region eu-central-1)
+	docker push $AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/front:$CIRCLE_SHA1
 }
 
 register_definition() {
